a728bb084b2238ac5ce783fcb640775af4eff397
jfourdachon <jfourdachon@yahoo.com>
2020-09-26 17:13:35 +0200

    integration overview done

--- src/components/TourCard.vue
+++ src/components/TourCard.vue
@@ -1,42 +1,46 @@
 <template>
-  <b-card class="card" no-body>
-    <div class="header">
-      <b-overlay :show="true" bg-color="#28b487" opacity="0.5" class="overlay">
-        <template v-slot:overlay><div /></template>
-        <b-card-img-lazy
-          :src="require(`@/assets/img/tours/${image}`)"
-          class="w-100 h-100"
-        />
-      </b-overlay>
-      <b-card-title class="card-title"
-        ><span>{{ name }}</span></b-card-title
-      >
-    </div>
-    <p class="mt-2 pl-3">
-      {{ difficulty | toUpperCase }} - {{ duration }} DAYS TOUR
-    </p>
-    <p class="mb-4 mt-3 pl-3">{{ summary }}</p>
-    <b-container>
-      <b-row align-v="center" class="pl-3">
-        <b-icon icon="geo-alt" variant="success"></b-icon>
-        <b-col class="pl-2" cols="5">{{ startLocation }}</b-col>
-        <b-icon icon="calendar2-event-fill" variant="success"></b-icon>
-        <b-col>{{ globalDate }}</b-col>
-      </b-row>
-      <b-row align-v="center" class="pl-3 py-2 mb-2">
-        <b-icon icon="flag" variant="success"></b-icon>
-        <b-col class="pl-2" cols="5">{{ stops }} stops</b-col>
-        <b-icon icon="person" variant="success"></b-icon>
-        <b-col>{{ maxGroupSize }}people</b-col>
-      </b-row>
+  <b-card class="card h-100" no-body>
+    <b-overlay :show="true" bg-color="#28b487" opacity="0.5" class="overlay">
+      <template v-slot:overlay><div /></template>
+      <b-card-img-lazy
+        :src="require(`@/assets/img/tours/${image}`)"
+        class="w-100 h-100"
+      />
+    </b-overlay>
+    <b-card-title class="card-title">
+      <span>{{ name }}</span>
+    </b-card-title>
+    <b-container class="px-4 h-100">
+      <b-col class="mt-2 upper">
+        {{ difficulty | toUpperCase }} - {{ duration }} DAYS TOUR
+      </b-col>
+      <b-col class="mb-4 mt-3 h-25 summary text-secondary">{{ summary }}</b-col>
+      <b-col>
+        <b-row align-v="center" class="pl-3">
+          <b-icon icon="geo-alt" variant="success"></b-icon>
+          <b-col class="pl-2 text-secondary" cols="5">{{
+            startLocation
+          }}</b-col>
+          <b-icon icon="calendar2-event-fill" variant="success"></b-icon>
+          <b-col class="text-secondary">{{ globalDate }}</b-col>
+        </b-row>
+        <b-row align-v="center" class="py-2 mb-2 pl-3">
+          <b-icon icon="flag" variant="success"></b-icon>
+          <b-col class="pl-2 text-secondary" cols="5">{{ stops }} stops</b-col>
+          <b-icon icon="person" variant="success"></b-icon>
+          <b-col class="text-secondary">{{ maxGroupSize }}people</b-col>
+        </b-row>
+      </b-col>
     </b-container>
     <b-card-footer>
-      <b-row>
+      <b-row class="px-4">
         <b-col cols="8">
-          <b-card-text>${{ price }} per person</b-card-text>
-          <b-card-text
-            >{{ ratingsAverage }} rating ({{ ratingsQuantity }})</b-card-text
-          >
+          <b-card-text class="text-secondary">
+            <strong>${{ price }}</strong> per person
+          </b-card-text>
+          <b-card-text class="text-secondary">
+            <strong>{{ ratingsAverage }}</strong> rating ({{ ratingsQuantity }})
+          </b-card-text>
         </b-col>
         <b-col align-self="center" cols="3">
           <b-row align-h="end">
@@ -49,31 +53,31 @@
 </template>
 
 <script>
-import moment from "moment";
+import moment from 'moment';
 export default {
-  name: "TourCard",
-  data: function () {
+  name: 'TourCard',
+  data: function() {
     return {
-      globalDate: "",
+      globalDate: '',
     };
   },
   props: [
-    "name",
-    "difficulty",
-    "price",
-    "duration",
-    "image",
-    "summary",
-    "maxGroupSize",
-    "ratingsAverage",
-    "ratingsQuantity",
-    "date",
-    "stops",
-    "startLocation",
+    'name',
+    'difficulty',
+    'price',
+    'duration',
+    'image',
+    'summary',
+    'maxGroupSize',
+    'ratingsAverage',
+    'ratingsQuantity',
+    'date',
+    'stops',
+    'startLocation',
   ],
-  mounted: function () {
+  mounted: function() {
     const date = new Date(this.date);
-    this.globalDate = moment(date).format("YYYY MMMM");
+    this.globalDate = moment(date).format('YYYY MMMM');
     console.log(this.globalDate);
   },
 };
@@ -81,4 +85,3 @@ export default {
 
 <style scoped lang="css" src="../assets/css/common.css"></style>
 <style scoped lang="css" src="../assets/css/card.css"></style>
-
